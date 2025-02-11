dd54e661647c738b6576cb2ae36eb1915fd58cb7
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2025-02-11 12:48:17 -0800

    fixed whilehover for desktop view in productgrid

--- ui/src/components/ProductGrid/ProductGrid.js
+++ ui/src/components/ProductGrid/ProductGrid.js
@@ -95,7 +95,8 @@ const ProductGrid = () => {
             key={product.id}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.2 }}
           >
             <Link to={`/product/${product.id}`}>
               <ProductImage src={product.image} alt={product.title} />
