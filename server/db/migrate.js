ea7bafe7b6e035f1e0cdae39fa8c0da1ad2283cb
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2025-02-11 17:56:10 -0800

    made changes to migration file

--- server/db/migrate.js
+++ server/db/migrate.js
@@ -48,11 +48,12 @@ async function migrate() {
     await client.execute(`
       CREATE TABLE IF NOT EXISTS order_items (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        order_id INTEGER,
+        order_id INTEGER NOT NULL,
         product_id INTEGER NOT NULL,
         quantity INTEGER NOT NULL,
         price_at_time INTEGER NOT NULL,
-        FOREIGN KEY (order_id) REFERENCES orders(id)
+        FOREIGN KEY (order_id) REFERENCES orders(id),
+        FOREIGN KEY (product_id) REFERENCES products(id)
       )
     `);
 
